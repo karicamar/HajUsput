c7026b0a3f25cc8c914e4bf22925b1fa19a852b9
karicamar <karicamar11@hotmail.com>
2023-10-03 23:44:34 +0200

    take value

--- HajUsputUI/hajusput_desktop/lib/screens/login_screen.dart
+++ HajUsputUI/hajusput_desktop/lib/screens/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:ionicons/ionicons.dart';
 
 import 'package:flutter/material.dart';
 
+TextEditingController _usernameController = new TextEditingController();
+    TextEditingController _passwordController = new TextEditingController();
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -13,56 +15,58 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
-  Widget inputField(String hint, IconData iconData){
-    
-    return Padding(padding: const EdgeInsets.all(16.0),
-    
-    child: SizedBox(
-      height: 40,
-      width: 300,
-      child: Material(
-        elevation: 8,
-        shadowColor: Colors.black87,
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(30),
-        child: TextField(
-          textAlignVertical: TextAlignVertical.bottom,
-          decoration: InputDecoration(
-        border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide.none,
-          ),
-        filled: true,
-        fillColor: Colors.white,
-        hintText: hint,
-        prefixIcon: Icon(iconData),
+  Widget inputField(
+      String hint, IconData iconData, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        height: 40,
+        width: 300,
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.black87,
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+          child: TextField(
+            textAlignVertical: TextAlignVertical.bottom,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: hint,
+              prefixIcon: Icon(iconData),
             ),
+            controller: controller,
           ),
+        ),
       ),
-   
-      ),
-    
     );
-  
   }
 
   Widget loginButton(String title) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      
-      child: SizedBox(height: 40, width: 100,
+      child: SizedBox(
+        height: 40,
+        width: 100,
         child: ElevatedButton(
           onPressed: () {
+            var username=_usernameController.text;
+            var password=_passwordController.text;
+
+          print("login $username $password");
             Navigator.of(context).push(
-          MaterialPageRoute(
-        builder: (context) => const HomeScreen(),
-          ),
-        );
-        },
+              MaterialPageRoute(
+                builder: (context) => const HomeScreen(),
+              ),
+            );
+          },
           style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 14),
-             backgroundColor: Colors.lightBlue,
+            backgroundColor: Colors.lightBlue,
             shape: const StadiumBorder(),
             elevation: 8,
             shadowColor: Colors.black87,
@@ -81,30 +85,30 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-body: Stack(children: [
-  //Image.network("https://e7.pngegg.com/pngimages/667/529/png-clipart-carpool-real-time-ridesharing-transport-passenger-car-driving-logo.png"),
-  
-  
-  Center(
-    child: Padding(
-      padding: const EdgeInsets.only(top:100),
-  
-      child: Column(
-        
-        mainAxisAlignment: MainAxisAlignment.center,
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          inputField('Userame', Ionicons.person_outline),
-          inputField('Password',Ionicons.lock_closed_outline),
-          loginButton('Login'),
-        ],
-      ),
-    ),
-  ),
-  
-],)
+    // TextEditingController _usernameController = new TextEditingController();
+    // TextEditingController _passwordController = new TextEditingController();
+    return Scaffold(
+        body: Stack(
+      children: [
+        //Image.network("https://e7.pngegg.com/pngimages/667/529/png-clipart-carpool-real-time-ridesharing-transport-passenger-car-driving-logo.png"),
 
-    );
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                inputField(
+                    'Username', Ionicons.person_outline, _usernameController),
+                inputField('Password', Ionicons.lock_closed_outline,
+                    _passwordController),
+                loginButton('Login'),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
