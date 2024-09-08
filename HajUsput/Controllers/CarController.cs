c397e2f307ffb37f72042375f8dbd9692d459513
karicamar <75027165+karicamar@users.noreply.github.com>
2024-09-08 23:39:09 +0200

    rerere

--- HajUsput/Controllers/CarController.cs
+++ HajUsput/Controllers/CarController.cs
@@ -12,8 +12,25 @@ namespace HajUsput.Controllers
     public class CarController : BaseCRUDController<Car, CarSearchObject, CarInsertRequest, CarUpdateRequest>
 
     {
-        public CarController(ILogger<BaseController<Car, CarSearchObject>> logger, ICarService service) : base(logger, service)
+        private readonly ICarService _carService;
+
+        public CarController(ILogger<BaseController<Car, hajUsput.Model.SearchObjects.CarSearchObject>> logger, ICarService service) : base(logger, service)
         {
+            _carService = service;
+
+        }
+        [HttpGet("user/{userId}")]
+        public IActionResult GetCarByUserId(int userId)
+        {
+            try
+            {
+                var car = _carService.GetCarsByUserId(userId);
+                return Ok(car);
+            }
+            catch (Exception ex)
+            {
+                return NotFound($"Car not found: {ex.Message}");
+            }
         }
     }
 }
